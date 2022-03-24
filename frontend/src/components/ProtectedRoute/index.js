f0d0eab03785a2cdb4392565db16cdca250ab851
Brittany Moliver <britt.moliver@gmail.com>
2022-03-23 21:56:47 -0400

    edited protected route to only show modal

--- frontend/src/components/ProtectedRoute/index.js
+++ frontend/src/components/ProtectedRoute/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 
+import { Modal } from "../../context/Modal";
+import LoginForm from "../LoginFormModal/LoginForm";
+import PageNotFound from "../PageNotFound/PageNotFound";
+
 const ProtectedRoute = (props) => {
   const user = useSelector((state) => state.session.user);
+
+  const [showModal, setShowModal] = useState(true);
+
   return (
-    <Route {...props}>{user ? props.children : <Redirect to="/login" />}</Route>
+    <Route {...props}>
+      {user ? (
+        props.children
+      ) : (
+        <>
+          <PageNotFound />
+          <div>
+            {showModal && (
+              <Modal onClose={() => setShowModal(false)}>
+                <LoginForm />
+              </Modal>
+            )}
+          </div>
+          {/* <Redirect to="/login" /> */}
+        </>
+      )}
+    </Route>
   );
 };
 
