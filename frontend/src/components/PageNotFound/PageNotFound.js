ade9d3e8306dce816a8208a6f7e9585960aa918c
Brittany Moliver <britt.moliver@gmail.com>
2022-03-23 21:40:08 -0400

    edited page not found

--- frontend/src/components/PageNotFound/PageNotFound.js
+++ frontend/src/components/PageNotFound/PageNotFound.js
@@ -1,11 +1,10 @@
+import panda from "../../images/404_panda.jpg"
+
 export default function PageNotFound() {
   return (
-    <>
-      <h1>Page Not Found!!!</h1>
-      <img
-        src="http://lh5.ggpht.com/_9F9_RUESS2E/SpV5Yi8Vv5I/AAAAAAAAA4E/W9-J8eMLokM/s800/50-Cool-and-Creative-404-Error-Pages-25.jpg"
-        alt="Page Not Found"
-      />
-    </>
+    <div className="panda">
+      {/* <h1>Page Not Found!!!</h1> */}
+      <img src={panda} alt="404 Page Not Found"/>
+    </div>
   );
 }
